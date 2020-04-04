b49047f37a50843bd8359f464a682ab435266000
vishal anand <vishal anand@vishal>
2020-04-04 18:20:47 +0530

    2nd commit

--- Git/src/com/GitTest.java
+++ Git/src/com/GitTest.java
@@ -4,7 +4,14 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println(Print());
 
+	}
+	
+	public static String Print() {
+		
+		
+		return "in the function print";
 	}
 
 }
